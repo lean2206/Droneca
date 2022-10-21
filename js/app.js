09ab12b793457e5324f9d5b93773ee1ddc509a8f
Leandro Saldivar <leandrosaldivar06@gmail.com>
2022-10-20 22:39:37 -0300

    archivos eliminados y renombrados

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/* Esta función se encarga de crear HTML para agregar un producto a la interfaz. Se debe proporcionar el nombre, precio, modelo y la ruta de la imágen del producto
+ */
+
+function addProd(nombre, precio, modelo, img){
+    
+    img = `<img class=new-prod src="${img}">`;
+    nom
+
+}
