ab41ea525d4ebf34f74c29240d0f956a7e59c942
Leandro Saldivar <leandrosaldivar06@gmail.com>
2022-12-03 21:52:01 -0300

    corrección compra finalizada

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ const cartOperation = (obj,currentUser) =>{
         confirmButtonText: 'Seguir Comprando',
     }).then((result) => {
         console.log(result)
-        if ((!result.isConfirmed && (result.dismiss!='backdrop'))) {
+        if ((!result.isConfirmed && (result.dismiss=="cancel"))) {
             
             Swal.fire({
                 title: `El total de tu compra es USD$${total}`,
